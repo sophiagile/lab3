a7ab3a8676af7218e2a2c3842c0dabd23c371565
sophiagile <156032706+sophiagile@users.noreply.github.com>
2024-03-06 21:12:36 -0500

    Add files via upload

---
+++ lab3.py
@@ -0,0 +1,68 @@
+def computepay(hours,rate) : 
+    #print("In computepay:", hours, rate)
+    if hours > 40: 
+        reg = rate * hours
+        otp = (hours - 40.0) * (rate *0.5)
+        pay = reg + otp 
+    else: 
+        pay = hours * rate 
+        #print("Returning",pay) 
+    return pay
+
+sh = input("Enter Hours: ")
+sr = input("Enter Rate: ")
+try:
+    fh = float(sh)
+    fr = float(sr)
+except:
+    print("Error, please enter numeric input")
+    quit()
+
+xp = computepay(fh, fr)
+
+print("Pay: ",xp)
+
+
+
+
+import random
+
+user_wins = 0 
+computer_wins = 0 
+options = ["rock", "paper", "scissors"]
+
+while True: 
+    user_input = input("Type Rock/Paper/Scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in options:
+      continue
+
+    random_number = random.randint(0, 2)
+    # rock: 0, paper: 1, scissors: 2
+    computer_pick = options[random_number]
+    print("Computer picked", computer_pick + ".")
+
+    if user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        user_wins += 1
+        continue
+
+    else:
+        print("You lost!")
+        computer_wins += 1
+
+print("You won", user_wins, "times.")
+print("The computer won", computer_wins, "times.")
+print("Goodbye!")
